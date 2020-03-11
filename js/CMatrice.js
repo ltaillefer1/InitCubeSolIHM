ac0b2e355443614da48e6a207edb99f569697035
ltaillefer1 <ltaillefer@btslivh.eu>
2020-03-11 17:37:28 +0100

    Modif Matrice 3

--- js/CMatrice.js
+++ js/CMatrice.js
@@ -35,9 +35,9 @@ CMatrice.prototype.modifPixelColor = function(pixel, valeur){
 	var G;
 	var B;
 
-	R = this.map(valeur, 255, 0, seuilHaut, seuilMilieu);
-	G = this.map(valeur, 0, 255, seuilMilieu, seuilBas);
-	B = this.map(valeur, 0, 255, seuilBas, seuilNull);
+	R = this.map(valeur, 255, 0, this.seuilHaut, this.seuilMilieu);
+	G = this.map(valeur, 255, 0, this.seuilMilieu, this.seuilBas);
+	B = this.map(valeur, 255, 0, this.seuilBas, this.seuilNull);
 
 	var couleur = '#'+('0'+R.toString(16)).slice(-2)+"00"+('0'+B.toString(16)).slice(-2)+"00"+('0'+G.toString(16)).slice(-2);
 
