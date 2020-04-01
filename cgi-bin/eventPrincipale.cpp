7cd4994d9f6fcb98bcd63045b56b8ec58f9b4fa2
ltaillefer1 <ltaillefer@btslivh.eu>
2020-04-01 17:09:22 +0200

    test event

--- cgi-bin/eventPrincipale.cpp
+++ cgi-bin/eventPrincipale.cpp
@@ -1,6 +1,9 @@
 #include "Lib.h"
 using namespace std;
 
+int min = 0;
+int max = 10;
+
 int main(){
 
 	int premiereValeurMagneto=(rand() % (4800 - (-4800) + 1)) - 4800;
@@ -8,10 +11,7 @@ int main(){
 	int troisiemeValeurMagneto=(rand() % (4800 - (-4800) + 1)) - 4800;
 	int number = 0;
 	int arr[64];
-	for (int i = 0; i < 64; i++){
-		arr[i] = number;
-		number++;
-	}
+	arr = newValue();
 
 	cout << "Content-Type: text/event-stream\r\n\r\n";
 	cout << "Cache-Control: no-cache\r\n\r\n";
@@ -22,96 +22,120 @@ int main(){
 		
 			cout << "Event: mesure\r\n\r\n";
 			cout << "data : "<<endl;*/			
+	for (int i = 0; i < 10 ;i++){
+		arr = newValue();	
+		cout << "event: matrice"<<"\n";
+		cout << "data : "<<"\"matrice\":[" << arr[0]<<","<< arr[1]<<","<< arr[2]<<","<< arr[3]<<","<< arr[4]<<","<< arr[5]<<","<< arr[6]<<","<< arr[7]<<","
+		<< arr[8]<<","<< arr[9]<<","<< arr[10]<<","<< arr[11]<<","<< arr[12]<<","<< arr[13]<<","<< arr[14]<<","<< arr[15]<<","
+		<< arr[16]<<","<< arr[17]<<","<< arr[18]<<","<< arr[19]<<","<< arr[20]<<","<< arr[21]<<","<< arr[22]<<","<< arr[23]<<","
+		<< arr[24]<<","<< arr[25]<<","<< arr[26]<<","<< arr[27]<<","<< arr[28]<<","<< arr[29]<<","<< arr[30]<<","<< arr[31]<<","
+		<< arr[32]<<","<< arr[33]<<","<< arr[34]<<","<< arr[35]<<","<< arr[36]<<","<< arr[37]<<","<< arr[38]<<","<< arr[39]<<","
+		<< arr[40]<<","<< arr[41]<<","<< arr[42]<<","<< arr[43]<<","<< arr[44]<<","<< arr[45]<<","<< arr[46]<<","<< arr[47]<<","
+		<< arr[48]<<","<< arr[49]<<","<< arr[50]<<","<< arr[51]<<","<< arr[52]<<","<< arr[53]<<","<< arr[54]<<","<< arr[55]<<","
+		<< arr[56]<<","<< arr[57]<<","<< arr[58]<<","<< arr[59]<<","<< arr[60]<<","<< arr[61]<<","<< arr[62]<<","<< arr[63]<<",";
+	}
 		
-			cout << "event: matrice"<<"\n";
-			cout << "data : " << for (int i = 0; i < 64; i++){arr[i]<<","};
-		
+	for (int i = 0; i < 10 ;i++){
+		cout << "event: etat"<<"\n";
+		cout << "data: " << "{\"stockage\": { "
 
-			cout << "event: etat"<<"\n";
-			cout << "data: " << "{\"stockage\": { "
+			// Stockage libre en Mo(Valeur comprise entre 0 et 100) 
+			<<"\"stockLibreMo\" :"<<rand()%101 << ","				
 
-				// Stockage libre en Mo(Valeur comprise entre 0 et 100) 
-				<<"\"stockLibreMo\" :"<<rand()%101 << ","				
-
-				//Stockage libre en pourcentage(Valeur comprise entre 0 et 100)
-				<<"\"stockLibreEnP\" :"<<rand()%101 << "},"			
+			//Stockage libre en pourcentage(Valeur comprise entre 0 et 100)
+			<<"\"stockLibreEnP\" :"<<rand()%101 << "},"			
 
 
-			//Ouverture de l'objet batterie dans le JSON
-			<<"\"batterie\" : {"
+		//Ouverture de l'objet batterie dans le JSON
+		<<"\"batterie\" : {"
 
-				//Le niveau de charge de la batterie en pourcentage(Valeur comprise entre 0 et 100)
-				<<"\"niveauDeCharge\" : "<<rand()%101<<","				
+			//Le niveau de charge de la batterie en pourcentage(Valeur comprise entre 0 et 100)
+			<<"\"niveauDeCharge\" : "<<rand()%101<<","				
 
-				//La tension de sortie de la batterie (Valeur comprise entre 0 et 100)
-				<<"\"tension\" : "<<rand()%101<<","				
+			//La tension de sortie de la batterie (Valeur comprise entre 0 et 100)
+			<<"\"tension\" : "<<rand()%101<<","				
 
-				//Le courant en sortie de batterie(Valeur comprise entre 0 et 100)
-				<<"\"courant\" : "<<rand()%101
-				<<"},"
+			//Le courant en sortie de batterie(Valeur comprise entre 0 et 100)
+			<<"\"courant\" : "<<rand()%101
+			<<"},"
 
 
-			//Ouverture de l'objet memoire
-			<<"\"memoire\" : { "
+		//Ouverture de l'objet memoire
+		<<"\"memoire\" : { "
 
-				//RAM disponible en Mo(Valeur comprise entre 0 et 100) 
-				<<"\"memoireDispoMo\" : " 
-				<<rand()%101<<","
+			//RAM disponible en Mo(Valeur comprise entre 0 et 100) 
+			<<"\"memoireDispoMo\" : " 
+			<<rand()%101<<","
 
-				//Occupation de la RAM en pourcentage(Valeur comprise entre 0 et 100)
-				<<"\"occupMemoire\" : "
-				<<rand()%101<<"},"
+			//Occupation de la RAM en pourcentage(Valeur comprise entre 0 et 100)
+			<<"\"occupMemoire\" : "
+			<<rand()%101<<"},"
 
-			//Ouverture de l'objet magneto dans le JSON
-			<<"\"magneto\" : {"
+		//Ouverture de l'objet magneto dans le JSON
+		<<"\"magneto\" : {"
 
-				//Le niveau de charge de la batterie en pourcentage(Valeur comprise entre 0 et 100)
-				//Magnetomètre
-            <<"\"ValeurMagnetoBX\":"
-            <<premiereValeurMagneto<<","
+			//Le niveau de charge de la batterie en pourcentage(Valeur comprise entre 0 et 100)
+			//Magnetomètre
+        <<"\"ValeurMagnetoBX\":"
+        <<premiereValeurMagneto<<","
 
-            //Magnetomètre
-            <<"\"ValeurMagnetoBY\":"
-            <<deuxiemeValeurMagneto<<","
+        //Magnetomètre
+        <<"\"ValeurMagnetoBY\":"
+        <<deuxiemeValeurMagneto<<","
 
-            //Magnetomètre
-            <<"\"ValeurMagnetoBZ\":"
-            <<troisiemeValeurMagneto<<"},"
+        //Magnetomètre
+        <<"\"ValeurMagnetoBZ\":"
+        <<troisiemeValeurMagneto<<"},"
 
-				//Ouverture de l'objet camera
-			<<"\"camera\" : { "
+			//Ouverture de l'objet camera
+		<<"\"camera\" : { "
 
-				<<"\"InfoCamera1\" : "<<rand()%101<<","
+			<<"\"InfoCamera1\" : "<<rand()%101<<","
 
-            	//Caméra
-            	<<"\"InfoCamera2\" : "<<rand()%101
-            	<<"},"
+        	//Caméra
+        	<<"\"InfoCamera2\" : "<<rand()%101
+        	<<"},"
 
-			//température0
-			<< "\"temp\":"
-			<< rand()%61<<","
+		//température0
+		<< "\"temp\":"
+		<< rand()%61<<","
 
-			//température1
-			<<"\"temp1\":"
-			<<rand()%61<<","
+		//température1
+		<<"\"temp1\":"
+		<<rand()%61<<","
 
-			//température2
-			<<"\"temp2\":"
-			<<rand()%61<<","
+		//température2
+		<<"\"temp2\":"
+		<<rand()%61<<","
 
-			//temperature3
-			<<"\"temp3\":"
-			<<rand()%61<<","
+		//temperature3
+		<<"\"temp3\":"
+		<<rand()%61<<","
 
-			//temperature4
-			<<"\"temp4\":"
-			<<rand()%61<<","
+		//temperature4
+		<<"\"temp4\":"
+		<<rand()%61<<","
 
-			//cameraIR
-			<<"\"cameraIR\":"
-			<<rand()%2
+		//cameraIR
+		<<"\"cameraIR\":"
+		<<rand()%2
 
-     << "}"<<"\r\n\r\n";
+ << "}"<<"\r\n\r\n";
+}
 
 	return 0;		
+}
+
+int newValue(){
+	int a[64];
+	if(max < 90){
+		for (int i = 0; i < 64; i++){
+			a[i] = number;
+			number = rand()%(max-min + 1) + min;
+		}
+	min += 10;
+	max += 10;
+	}else {
+		return a;
+	}
 }
