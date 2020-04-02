6d0298e15616ac72d3876a0c7a3e640092f97925
ltaillefer1 <ltaillefer@btslivh.eu>
2020-04-02 16:19:18 +0200

    test js

--- js/matrice/mainMatrice.js
+++ js/matrice/mainMatrice.js
@@ -26,7 +26,7 @@ function draw() {
 }
 
 function update() { 
-    document.addEventListener("DOMcontentLoaded" ,function(event) { 
+    document.addEventListener("DOMC<ontentLoaded" ,function(event) { 
         source.addEventListener("matrice", function(evt){
             var obj = JSON.parse(evt.data);
             console.log(obj.matrice);
