a7e5b19ee47800172aba44f79a666884d6090ab5
ltaillefer1 <ltaillefer@btslivh.eu>
2020-04-29 15:23:24 +0200

    fixing error

--- InitCubeServer/InitCubeServeur.cpp
+++ InitCubeServer/InitCubeServeur.cpp
@@ -93,95 +93,95 @@ string InitCubeServeur::genFakeTram(){
         }
     }
 
-    testInstru +=  std::string("]}}"+"\r\n\r\n");
+    testInstru +=  std::string("]}}\r\n\r\n");
 
     string testEtat =   std::string("{\"etat\":{  \"stockage\": { ") 
 
                     // Stockage libre en Mo(Valeur comprise entre 0 et 100) 
-                    + "\"stockLibreMo\" :" + rand()%101 + ","               
+                     "\"stockLibreMo\" :"  rand()%101  ","               
 
                     //Stockage libre en pourcentage(Valeur comprise entre 0 et 100)
-                    + "\"stockLibreEnP\" :"+ rand()%101 + "},"         
+                     "\"stockLibreEnP\" :" rand()%101 + "},"         
 
 
                 //Ouverture de l'objet batterie dans le JSON
-                +"\"batterie\" : {"
+                "\"batterie\" : {"
 
                     //Le niveau de charge de la batterie en pourcentage(Valeur comprise entre 0 et 100)
-                    +"\"niveauDeCharge\" : "+rand()%101+","              
+                    "\"niveauDeCharge\" : "rand()%101 ","              
 
                     //La tension de sortie de la batterie (Valeur comprise entre 0 et 100)
-                    +"\"tension\" : "+rand()%101+","             
+                    "\"tension\" : "rand()%101","             
 
                     //Le courant en sortie de batterie(Valeur comprise entre 0 et 100)
-                    +"\"courant\" : "+rand()%101
-                    +"},"
+                    "\"courant\" : "rand()%101
+                    "},"
 
 
                 //Ouverture de l'objet memoire
-                +"\"memoire\" : { "
+                "\"memoire\" : { "
 
                     //RAM disponible en Mo(Valeur comprise entre 0 et 100) 
-                    +"\"memoireDispoMo\" : " 
-                    +rand()%101+","
+                    "\"memoireDispoMo\" : " 
+                    rand()%101","
 
                     //Occupation de la RAM en pourcentage(Valeur comprise entre 0 et 100)
-                    +"\"occupMemoire\" : "
-                    +rand()%101+"},"
+                    "\"occupMemoire\" : "
+                    rand()%101"},"
 
                 //Ouverture de l'objet magneto dans le JSON
-                +"\"magneto\" : {"
+                "\"magneto\" : {"
 
                     //Le niveau de charge de la batterie en pourcentage(Valeur comprise entre 0 et 100)
                     //Magnetomètre
-                    +"\"ValeurMagnetoBX\":"
-                    +premiereValeurMagneto+","
+                    "\"ValeurMagnetoBX\":"
+                    premiereValeurMagneto","
 
                     //Magnetomètre
-                    +"\"ValeurMagnetoBY\":"
-                    +deuxiemeValeurMagneto+","
+                    "\"ValeurMagnetoBY\":"
+                    deuxiemeValeurMagneto","
 
                     //Magnetomètre
-                    +"\"ValeurMagnetoBZ\":"
-                    +troisiemeValeurMagneto+"},"
+                    "\"ValeurMagnetoBZ\":"
+                    troisiemeValeurMagneto"},"
 
                     //Ouverture de l'objet camera
-                +"\"camera\" : { "
+                "\"camera\" : { "
 
-                    +"\"InfoCamera1\" : "+rand()%101+","
+                    "\"InfoCamera1\" : "rand()%101","
 
                     //Caméra
-                    +"\"InfoCamera2\" : "+rand()%101
-                    +"},"
+                    "\"InfoCamera2\" : "rand()%101
+                    "},"
                 //ouverture de l'objet temperature Systeme
-                +"\"temperatureSys\" : { "
+                "\"temperatureSys\" : { "
 
                     //température0
-                    + "\"temp\":"
-                    + rand()%61+","
+                     "\"temp\":"
+                     rand()%61+","
 
                     //température1
-                    +"\"temp1\":"
-                    +rand()%61+","
+                    "\"temp1\":"
+                    rand()%61+","
 
                     //température2
-                    +"\"temp2\":"
-                    +rand()%61+","
+                    "\"temp2\":"
+                    rand()%61","
 
                     //temperature3
-                    +"\"temp3\":"
-                    +rand()%61+","
+                    "\"temp3\":"
+                    rand()%61","
 
                     //temperature4
-                    +"\"temp4\":"
-                    +rand()%61
-                    +"},"
+                    "\"temp4\":"
+                    rand()%61
+                    "},"
          
                 //cameraIR
-                +"\"cameraIR\":"
-                +rand()%2
+                "\"cameraIR\":"
+                rand()%2
 
-            + "}}"+"\r\n\r\n";
+             "}}\r\n\r\n";
 
     string repaire = "kohngdvbe";
 
