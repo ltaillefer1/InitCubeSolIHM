01da53699b649fee239298724b3dc5692e6ef426
ltaillefer1 <ltaillefer@btslivh.eu>
2020-05-06 13:43:33 +0200

    test pour fix error de file non trouve

--- cgi-bin/json.hpp
+++ cgi-bin/json.hpp
@@ -49,28 +49,28 @@ SOFTWARE.
 #include <vector> // vector
 
 #include "nlohmann/adl_serializer.hpp"
-#include <nlohmann/detail/conversions/from_json.hpp>
-#include <nlohmann/detail/conversions/to_json.hpp>
-#include <nlohmann/detail/exceptions.hpp>
-#include <nlohmann/detail/input/binary_reader.hpp>
-#include <nlohmann/detail/input/input_adapters.hpp>
-#include <nlohmann/detail/input/lexer.hpp>
-#include <nlohmann/detail/input/parser.hpp>
-#include <nlohmann/detail/iterators/internal_iterator.hpp>
-#include <nlohmann/detail/iterators/iter_impl.hpp>
-#include <nlohmann/detail/iterators/iteration_proxy.hpp>
-#include <nlohmann/detail/iterators/json_reverse_iterator.hpp>
-#include <nlohmann/detail/iterators/primitive_iterator.hpp>
-#include <nlohmann/detail/json_pointer.hpp>
-#include <nlohmann/detail/json_ref.hpp>
-#include <nlohmann/detail/macro_scope.hpp>
-#include <nlohmann/detail/meta/cpp_future.hpp>
-#include <nlohmann/detail/meta/type_traits.hpp>
-#include <nlohmann/detail/output/binary_writer.hpp>
-#include <nlohmann/detail/output/output_adapters.hpp>
-#include <nlohmann/detail/output/serializer.hpp>
-#include <nlohmann/detail/value_t.hpp>
-#include <nlohmann/json_fwd.hpp>
+#include "nlohmann/detail/conversions/from_json.hpp"
+#include "nlohmann/detail/conversions/to_json.hpp"
+#include "nlohmann/detail/exceptions.hpp"
+#include "nlohmann/detail/input/binary_reader.hpp"
+#include "nlohmann/detail/input/input_adapters.hpp"
+#include "nlohmann/detail/input/lexer.hpp"
+#include "nlohmann/detail/input/parser.hpp"
+#include "nlohmann/detail/iterators/internal_iterator.hpp"
+#include "nlohmann/detail/iterators/iter_impl.hpp"
+#include "nlohmann/detail/iterators/iteration_proxy.hpp"
+#include "nlohmann/detail/iterators/json_reverse_iterator.hpp"
+#include "nlohmann/detail/iterators/primitive_iterator.hpp"
+#include "nlohmann/detail/json_pointer.hpp"
+#include "nlohmann/detail/json_ref.hpp"
+#include "nlohmann/detail/macro_scope.hpp"
+#include "nlohmann/detail/meta/cpp_future.hpp"
+#include "nlohmann/detail/meta/type_traits.hpp"
+#include "nlohmann/detail/output/binary_writer.hpp"
+#include "nlohmann/detail/output/output_adapters.hpp"
+#include "nlohmann/detail/output/serializer.hpp"
+#include "nlohmann/detail/value_t.hpp"
+#include "nlohmann/json_fwd.hpp"
 
 /*!
 @brief namespace for Niels Lohmann
@@ -8090,7 +8090,7 @@ namespace std
 
 /// hash value for JSON objects
 template<>
-struct hash<nlohmann::json>
+struct hash"nlohmann::json>
 {
     /*!
     @brief return a hash value for a JSON object
@@ -8100,7 +8100,7 @@ struct hash<nlohmann::json>
     std::size_t operator()(const nlohmann::json& j) const
     {
         // a naive hashing via the string representation
-        const auto& h = hash<nlohmann::json::string_t>();
+        const auto& h = hash"nlohmann::json::string_t>();
         return h(j.dump());
     }
 };
@@ -8128,9 +8128,9 @@ struct less<::nlohmann::detail::value_t>
 @since version 1.0.0
 */
 template<>
-inline void swap<nlohmann::json>(nlohmann::json& j1, nlohmann::json& j2) noexcept(
-    is_nothrow_move_constructible<nlohmann::json>::value and
-    is_nothrow_move_assignable<nlohmann::json>::value
+inline void swap"nlohmann::json>(nlohmann::json& j1, nlohmann::json& j2) noexcept(
+    is_nothrow_move_constructible"nlohmann::json>::value and
+    is_nothrow_move_assignable"nlohmann::json>::value
 )
 {
     j1.swap(j2);
@@ -8176,6 +8176,6 @@ inline nlohmann::json::json_pointer operator "" _json_pointer(const char* s, std
     return nlohmann::json::json_pointer(std::string(s, n));
 }
 
-#include <nlohmann/detail/macro_unscope.hpp>
+#include "nlohmann/detail/macro_unscope.hpp"
 
 #endif  // INCLUDE_NLOHMANN_JSON_HPP_
